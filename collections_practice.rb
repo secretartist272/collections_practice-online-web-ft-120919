0ed1dc0de4a90e2c0ef4c30172cfdb97aaa545e6
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 03:18:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort! {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  result = array.sort {|left, right| left.length <=> right.length}
+  return result
+end
+
+def swap_elements(array)
+   array[1], array[2] = array[2], array[1]
+   array
+end
+
+swap_elements(["blake", "ashley", "scott"]) 
 
